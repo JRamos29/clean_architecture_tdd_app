5d1afb8f536cb7b4992d81a26986939fc2ec2895
JRamos29 <malebolge@gmail.com>
2021-12-16 23:19:33 -0300

    Implemented GetRandomNumberTrivia with tests.

--- lib/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart
@@ -1,9 +1,9 @@
-import 'package:clean_architecture_tdd_app/core/error/failures.dart';
-import 'package:clean_architecture_tdd_app/core/use_cases/use_case.dart';
-import 'package:clean_architecture_tdd_app/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../../core/error/failures.dart';
+import '../../../../core/use_cases/use_case.dart';
+import '../entities/number_trivia.dart';
 import '../repositories/number_trivia_repository.dart';
 
 class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
