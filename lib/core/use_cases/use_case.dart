94538550385cfa18449c51de86a151396911aac8
JRamos29 <malebolge@gmail.com>
2021-12-17 16:46:32 -0300

    Initial impleentation for NumberTriviaRepositoryImpl, NumberTriviaRepositoryImpl tests, NumberTriviaLocalDataSource, NetworkInfo and Exception

--- lib/core/use_cases/use_case.dart
+++ lib/core/use_cases/use_case.dart
@@ -1,6 +1,7 @@
-import 'package:clean_architecture_tdd_app/core/error/failures.dart';
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+
+import '../error/failure.dart';
 
 abstract class UseCase<Type, Params> {
   Future<Either<Failure, Type>> call(Params params);
