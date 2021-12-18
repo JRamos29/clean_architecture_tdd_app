9a7eb13b45525d3e6d3ba0c4896a65e07162bc73
JRamos29 <malebolge@gmail.com>
2021-12-17 23:23:18 -0300

    Initial implementation for NumberTrivia bloc. Implemented input_converter with tests.

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
@@ -0,0 +1,16 @@
+part of 'number_trivia_bloc.dart';
+
+@immutable
+abstract class NumberTriviaEvent extends Equatable {}
+
+class GetTriviaForConcreteNumber extends NumberTriviaEvent {
+  final String numberString;
+
+  GetTriviaForConcreteNumber(this.numberString);
+
+  List<Object> get props => [numberString];
+}
+
+class GetTriviaForRandomNumber extends NumberTriviaEvent {
+  List<Object> get props => [];
+}
