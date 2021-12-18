5368e89aa591b9635f84e175b4f8cdbd87f1b26d
JRamos29 <malebolge@gmail.com>
2021-12-17 23:30:02 -0300

    Initial implementation for NumberTriviaState (Bloc).

--- lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -1,6 +1,28 @@
 part of 'number_trivia_bloc.dart';
 
 @immutable
-abstract class NumberTriviaState {}
+abstract class NumberTriviaState extends Equatable {}
 
-class NumberTriviaInitial extends NumberTriviaState {}
+class Empty extends NumberTriviaState {
+  List<Object> get props => [];
+}
+
+class Loading extends NumberTriviaState {
+  List<Object> get props => [];
+}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  Loaded({required this.trivia});
+
+  List<Object> get props => [trivia];
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  Error({required this.message});
+
+  List<Object> get props => [message];
+}
